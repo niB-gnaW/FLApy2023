c1af1a91c7e00acdf3257dbd6f0bb421913cb877
Wang Bin <wb931022@hotmail.com>
2023-12-16 17:05:09 +0800

    231215 update official version 'JEAN' V1.3.1 LTS
    -Bin Wang checked
    -Codecov test
    -Bug fixed

--- tests/test_DataManagement.py
+++ tests/test_DataManagement.py
@@ -3,6 +3,8 @@ import requests
 import os
 import pytest
 import numpy as np
+import xarray as xr
+import random
 
 def download_file(url, local_filename):
     with requests.get(url, stream=True) as r:
@@ -28,6 +30,14 @@ def demo_data_raster():
     yield filename
     os.remove(filename)
 
+@pytest.fixture(scope="module")
+def demo_data_csv():
+    url = 'https://raw.githubusercontent.com/niB-gnaW/FLApy2023/master/demo_Data/obsGround_6.csv'
+    filename = 'obsGround_6.csv'
+    download_file(url, filename)
+    yield filename
+    os.remove(filename)
+
 
 
 
@@ -42,4 +52,23 @@ def test_read_RasterData(demo_data_raster):
     inRasterFile = demo_data_raster
     site = fp.DataManagement.StudyFieldLattice()
     site.read_RasterData(inRasterFile)
-    assert isinstance(site.raster_data, np.ndarray) is True and site.raster_data.shape[1] == 3
+    assert isinstance(site.DSM, xr.DataArray) is True
+
+def test_m2p():
+    one_mesh = xr.DataArray(np.ones((10, 10)), dims=('x', 'y'))
+    outPoints = fp.DataManagement.StudyFieldLattice.m2p(one_mesh)
+    assert isinstance(outPoints, np.ndarray) is True and outPoints.shape[1] == 3
+
+def test_p2m():
+    X = np.arange(0, 10, 1)
+    Y = np.arange(0, 10, 1)
+    XY = np.meshgrid(X, Y)
+    one_points = np.vstack((XY[0].flatten(), XY[1].flatten(), np.ones((100,)))).T
+    outMesh = fp.DataManagement.StudyFieldLattice.p2m(one_points, 1)
+    assert isinstance(outMesh, xr.DataArray) is True
+
+def test_read_csvData():
+    inCSVFile = 'obsGround_6.csv'
+    site = fp.DataManagement.StudyFieldLattice()
+    site.read_csvData(inCSVFile)
+    assert isinstance(site.obsGround, np.ndarray) is True and site.obsGround.shape[1] == 3
